f88a62e8b7f1da0fe6bc5ec55348e06d93f4677d
Abdessamad Mahjoubi <abdessamad.mahjoubi@etixway.com>
2026-02-22 01:33:43 +0100

    style: formatage automatique du code terraform

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,44 +17,44 @@ locals {
 
 
 resource "azurerm_resource_group" "main" {
-  name = "rg-${var.project_name}-${var.environment}-${var.location}"
+  name     = "rg-${var.project_name}-${var.environment}-${var.location}"
   location = var.location
-  tags = local.common_tags
+  tags     = local.common_tags
 }
 
 module "network" {
-    # Source : Où est le code du module ?
-    source = "../../modules/network"
+  # Source : Où est le code du module ?
+  source = "../../modules/network"
 
-    #  Inputs : On passe les valeurs
+  #  Inputs : On passe les valeurs
   # Note : On utilise resource_group.main.name pour créer une dépendance explicite
 
   resource_group_name = azurerm_resource_group.main.name
-  location = var.location
+  location            = var.location
 
 
   # On peut surcharger le nom si on veut, ou laisser le défaut
   vnet_name = "vnet-${var.project_name}-${var.environment}"
-  tags = local.common_tags
-  
+  tags      = local.common_tags
+
 }
 
 
 module "compute" {
 
-    source = "../../modules/compute"
+  source = "../../modules/compute"
 
-    # On passe les infos de base
-    resource_group_name = azurerm_resource_group.main.name
-    location = var.location
+  # On passe les infos de base
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
 
-    # MAGIE TERRAFORM : On connecte la sortie du module Network à l'entrée du module Compute
-    subnet_id = module.network.public_subnet_id
+  # MAGIE TERRAFORM : On connecte la sortie du module Network à l'entrée du module Compute
+  subnet_id = module.network.public_subnet_id
 
-    # Optionnel : On peut surcharger le nom
-    vm_name = "vm-portfolio-dev"
-    tags = local.common_tags
-  
+  # Optionnel : On peut surcharger le nom
+  vm_name = "vm-portfolio-dev"
+  tags    = local.common_tags
+
 }
 
 # On veut afficher l'IP dans le terminal à la fin
@@ -63,7 +63,7 @@ output "web_server_ip" {
 }
 
 output "app_url" {
-  value = "http://${module.compute.public_ip_address}"
+  value       = "http://${module.compute.public_ip_address}"
   description = "L'URL pour accéder à l'application"
 }
 
